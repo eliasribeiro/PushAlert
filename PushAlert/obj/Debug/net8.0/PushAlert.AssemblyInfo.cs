8313303b55f14f8c30b76183f0f53cb9111e7f73
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2024-11-02 12:12:43 -0300

    Config

--- PushAlert/obj/Debug/net8.0/PushAlert.AssemblyInfo.cs
+++ PushAlert/obj/Debug/net8.0/PushAlert.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PushAlert")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e21bdb7176b1da1e3d8a2b85f5acfd52502d96b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0fb4334dcd2690f8fb1afa8cba1f4b90ad2c5e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PushAlert")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PushAlert")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
